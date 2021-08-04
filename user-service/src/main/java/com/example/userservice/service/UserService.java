b71848e0edfb41f6c743f52a5b66914028ce4f8c
unknown <gshgsh0831@fasoo.com>
2021-08-04 23:27:00 +0900

    user-service

---
+++ user-service/src/main/java/com/example/userservice/service/UserService.java
@@ -0,0 +1,9 @@
+package com.example.userservice.service;
+
+import com.example.userservice.dto.UserDto;
+import org.apache.catalina.User;
+
+public interface UserService {
+
+    UserDto createUser(UserDto userDto);
+}
